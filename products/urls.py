cac51e4a088e58b7488bc72b30bf063ac2dcea47
fatemehkarimi <fatemehkarimi.1998@yahoo.com>
2020-03-05 19:31:15 +0330

    add search for products

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import ProductBySubCategoryListView
+from .views import (
+    ProductBySubCategoryListView,
+    ProductDetailView,
+    ProductSearchResultListView,
+)
 
 urlpatterns = [
     path('by/subcategory/',
         ProductBySubCategoryListView.as_view(), name='product_subcat'),
+    path('search/result',
+        ProductSearchResultListView.as_view(), name='product_search_result'),
+    path('detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views import View
+from django.db.models import Q
+from django.views.generic import ListView, DetailView
 
-from .models import GeneralProduct
+from .models import (
+    GeneralProduct,
+    UnCountableProduct,
+    CountableProduct
+)
 
 # Create your views here.
 class ProductBySubCategoryListView(ListView):
@@ -11,4 +17,37 @@ class ProductBySubCategoryListView(ListView):
 
     def get_queryset(self):
         subcat = self.request.GET.get('subcat')
-        return GeneralProduct.objects.filter(subcategory__subcategory_name=subcat)
+        return GeneralProduct.objects.filter(
+            subcategory__subcategory_name=subcat
+        )
+
+
+class ProductDetailView(DetailView):
+    model = GeneralProduct
+    template_name = 'products/product_detail.html'
+    cotext_object_name = 'general_product_info'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        if self.object.is_countable:
+            context['countable_product_info'] = CountableProduct.objects.get(
+                pk=self.object.pk)
+        else:
+            context['uncountable_product_info'] = UnCountableProduct.objects.get(
+                pk=self.object.pk)
+        return context
+
+
+class ProductSearchResultListView(ListView):
+    model = GeneralProduct
+    context_object_name = 'matched_products'
+    template_name = 'products/product_search_result.html'
+
+    def get_queryset(self):
+        name = self.request.GET.get('product_name')
+        return GeneralProduct.objects.filter(
+            Q(product_name__icontains=name)
+            | Q(subcategory__subcategory_name__icontains=name)
+            | Q(subcategory__parent_category__category_name__icontains=name))
+
+    
