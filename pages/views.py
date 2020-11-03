09337774130767876a13fff13ededab848ba3fa3
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2020-11-03 23:08:57 +0330

    [COMPLETE] homepage

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from products.models import GeneralProduct
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = GeneralProduct.objects.filter().order_by('-total_available_in_stock')[:10]
+        return context
+    
 
 
 def AboutPageView(request):
