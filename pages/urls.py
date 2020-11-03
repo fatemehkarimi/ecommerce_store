fb52a0e986da60efa3eddbae34760e27232c470b
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2020-11-03 15:00:39 +0330

    [ADD] about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about', AboutPageView, name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+def AboutPageView(request):
+    return render(request, 'about.html')
